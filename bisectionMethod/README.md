4f699fcd1fd2663d5dd34374078a8a72a98ebb07
samuellando <samuel.lando@mail.concordia.ca>
2019-09-11 16:37:07 -0400

    Day 11

---
+++ bisectionMethod/README.md
@@ -0,0 +1,2 @@
+Write a function that determines the solution of f(x) = 0 on a given interval
+[a,b]
---
+++ bisectionMethod/sol.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <math.h> 
+
+double f(double x) { 
+  return x+1/x-100; 
+} 
+
+double bisectionMethod(double a, double b, int n) { 
+  double mid = (a+b)/2;
+  printf("n = %d : f(%.16f) = %.16f : f(%.16f) = %.16f : f(%.16f) = %.16f\n", n, a, f(a), b, f(b), mid, f(mid));
+  if (n == 1) return mid;
+  if (f(a)*f(mid) < 0) return bisectionMethod(a, mid, n-1);
+  else return bisectionMethod(mid, b, n-1);
+}
+
+int main() {
+  double a = 0.008;
+  double b = 0.012;
+  int n = 7;
+  double error = (b-a)/pow(2, n-1);
+  double sol = bisectionMethod(a,b,n);
+  printf("f(%.16f) = %.16f\n", sol, f(sol));
+  printf("error: %.16e\n", error);
+  printf("%.16f - %.16f\n", sol-error, sol+error);
+  return 0;
+}
