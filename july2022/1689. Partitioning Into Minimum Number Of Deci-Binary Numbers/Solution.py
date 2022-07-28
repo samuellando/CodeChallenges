21c6e6ff51683ab1f6bcddfa4ec0893ed974de89
Samuel Lando <samuel.lando@aol.com>
2022-07-28 10:36:15 -0400

    1689

---
+++ july2022/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.minPartitions("123456789012334223456"))
