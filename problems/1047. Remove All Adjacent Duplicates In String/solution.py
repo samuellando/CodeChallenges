a28a1544ff8b1ec2e1e4e2009196f3efd077cb10
Samuel Lando <samuel.lando@aol.com>
2022-11-10 11:29:16 -0500

    1047.

---
+++ problems/1047. Remove All Adjacent Duplicates In String/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        i = 0
+        while i < len(s) - 1:
+            if s[i] == s[i+1]:
+                s = s[:i] + s[i+2:]
+                i -= 2
+        
+            i += 1
+            if i < 0:
+                i = 0
+        
+        return s
