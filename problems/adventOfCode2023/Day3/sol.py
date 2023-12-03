0c723f6c2d3dac6ab65f629e19cdbf82f6a6aca5
Samuel Lando <samuel.lando@aol.com>
2023-12-03 12:22:37 -0500

    Day 3

---
+++ problems/adventOfCode2023/Day3/sol.py
@@ -0,0 +1,45 @@
+"""
+Get all the numbers (n, y, xstart, xend)
+Get all the symbols (y, x)
+For each symobl check if there is a adgecent number
+"""
+
+import sys
+with open(sys.argv[1], 'r') as f:
+    lines = f.readlines()
+    
+    numbers = {}
+    for y, l in enumerate(lines):
+        xstart = -1
+        xend = -1
+        for x, c in enumerate(l):
+            if c.isdigit():
+                if xstart == -1:
+                    xstart = x
+                xend = x
+            else:
+                if xstart != -1:
+                    n = int(l[xstart:xend+1])
+                    numbers[y] = numbers.get(y, []) + [[n, xstart, xend, False]]
+                    xstart = -1
+                    xend = -1
+
+    symbols = []
+    for y, l in enumerate(lines):
+        for x, c in enumerate(l):
+            if not c.isdigit() and not c == '.' and not c == "\n":
+                symbols.append((y,x))
+
+    sum = 0
+    for y, x in symbols:
+        for yp in range(y-1, y+2):
+            for i, v in enumerate(numbers.get(yp, [])):
+                xstart = v[1]
+                xend = v[2]
+                n = v[0]
+                used = v[3]
+                if not used and x >= xstart - 1 and x <= xend + 1:
+                    sum += n
+                    v[3] = True
+                    
+    print(sum)
